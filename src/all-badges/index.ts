ad386e89cda4a85f67afe3d2a6296ca990937a19
Anton Medvedev <anton@medv.io>
2023-10-05 22:20:52 +0200

    Rearrange badges

--- src/all-badges/index.ts
+++ src/all-badges/index.ts
@@ -1,8 +1,8 @@
 export const allBadges = [
   await import('./abc-commit/abc-commit.js'),
+  await import('./stars/stars.js'),
   await import('./dead-commit/dead-commit.js'),
   await import('./fuck-commit/fuck-commit.js'),
   await import('./mass-delete-commit/mass-delete-commit.js'),
   await import('./revert-revert-commit/revert-revert-commit.js'),
-  await import('./stars/stars.js'),
 ] as const
