0ec14137949f813fb174b2230795bf4fe800f171
Aleksandr Sysoev <sanchous.ok@gmail.com>
2023-10-08 00:50:39 +0700

    Add `my-badges contributor` badge

--- src/all-badges/index.ts
+++ src/all-badges/index.ts
@@ -8,4 +8,5 @@ export const allBadges = [
   await import('./fuck-commit/fuck-commit.js'),
   await import('./mass-delete-commit/mass-delete-commit.js'),
   await import('./revert-revert-commit/revert-revert-commit.js'),
+  await import('./my-badges-contributor/my-badges-contributor.js'),
 ] as const
