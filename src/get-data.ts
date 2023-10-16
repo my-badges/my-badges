36b9b8917ff54c9407220aeb7dcd37ef2f8c3481
Anton Medvedev <anton@medv.io>
2023-10-16 11:59:58 +0200

    Remove old imports

--- src/get-data.ts
+++ src/get-data.ts
@@ -1,9 +1,6 @@
 import { collect, Data } from './collect/collect.js'
 import fs from 'node:fs'
-import { Badge } from './badges.js'
-import { Octokit, RequestError } from 'octokit'
-import { decodeBase64 } from './utils.js'
-import { MY_BADGES_JSON_PATH } from './constants.js'
+import { Octokit } from 'octokit'
 
 export async function getData(
   octokit: Octokit,
