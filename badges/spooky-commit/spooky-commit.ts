ef56165852597a70fe8de3bc8740bc6ac6c5e6b0
Vatsal Sanjay <vatsalsanjay@gmail.com>
2024-11-26 18:19:35 +0100

    remove log
    do not need the console...

--- badges/spooky-commit/spooky-commit.ts
+++ badges/spooky-commit/spooky-commit.ts
@@ -9,7 +9,6 @@ export default define({
     for (const repo of data.repos) {
       for (const commit of repo.commits) {
         const data = new Date(commit.committedDate)
-        console.log('Date:', data, 'Month:', data.getMonth(), 'Day:', data.getDate())
         if (data.getMonth() === 9 && data.getDate() === 31) {
           commits.push(commit)
         }
