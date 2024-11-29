e326eec4e490d59b958cf426b78c936cd5989b32
Anton Golub <antongolub@antongolub.com>
2024-11-29 22:56:14 +0300

    refactor: rewrite repo fetcher with zurk

--- src/repo.ts
+++ src/repo.ts
@@ -1,56 +1,49 @@
 import fs from 'node:fs'
-import { chdir } from 'node:process'
+import path from 'node:path'
 import { Badge } from './badges.js'
-import { exec, execWithOutput } from './utils.js'
+import { $ as _, type TShellSync } from './utils.js'
 
-export function syncRepo(owner: string, repo: string, token: string) {
-  if (fs.existsSync('repo')) {
-    chdir('repo')
-    exec('git', ['pull'])
-    chdir('..')
+const MY_BADGES_NAME = 'My Badges'
+const MY_BADGES_EMAIL = 'my-badges@users.noreply.github.com'
+const MY_BADGES_FILE = 'my-badges/my-badges.json'
+const CWD = process.cwd()
+const DIR = 'repo'
+
+export const __internal__ = {cwd: CWD}
+const $ = ((...args: any) => _({
+  cwd: path.resolve(__internal__.cwd, DIR),
+  sync: true
+})(...args)) as TShellSync
+
+export function syncRepo(owner: string, repo: string, token?: string) {
+  const {cwd} = __internal__
+  if (fs.existsSync(path.resolve(cwd, DIR))) {
+    $`git pull`
     return
   }
+  const auth = token ? `${owner}:${token}@` : ''
 
-  exec('git', [
-    'clone',
-    '--depth=1',
-    `https://${owner}:${token}@github.com/${owner}/${repo}.git`,
-    'repo',
-  ])
-
-  chdir('repo')
-
-  exec('git', ['config', 'user.name', 'My Badges'])
-  exec('git', ['config', 'user.email', 'my-badges@users.noreply.github.com'])
-
-  chdir('..')
+  $({cwd})`git clone --depth=1 https://${auth}github.com/${owner}/${repo}.git ${DIR}`
+  $`git config user.name ${MY_BADGES_NAME}`
+  $`git config user.email ${MY_BADGES_EMAIL}`
 }
 
 export function thereAreChanges(): boolean {
-  chdir('repo')
-
-  const changes = execWithOutput('git', ['status', '--porcelain']).trim()
-
-  chdir('..')
-
-  return changes !== ''
+  return $`git status --porcelain`.stdout.trim() !== ''
 }
 
 export function gitPush() {
-  chdir('repo')
-
-  exec('git', ['add', '.'])
-  exec('git', ['status'])
-  exec('git', ['commit', '-m', 'Update badges'])
-  exec('git', ['push'])
-
-  chdir('..')
+  $`git add .`
+  $`git status`
+  $`git commit -m Update badges`
+  $`git push`
 }
 
 export function getUserBadges(): Badge[] {
-  if (!fs.existsSync('repo/my-badges/my-badges.json')) {
+  const file = path.resolve(__internal__.cwd, DIR, MY_BADGES_FILE)
+  if (!fs.existsSync(file)) {
     return []
   }
-  const data = fs.readFileSync('repo/my-badges/my-badges.json', 'utf8')
+  const data = fs.readFileSync(file, 'utf8')
   return JSON.parse(data)
 }
