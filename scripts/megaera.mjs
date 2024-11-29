30e6faee3bbd284c8080437c5b2473c67b3574f7
Anton Golub <antongolub@antongolub.com>
2024-11-29 22:59:16 +0300

    style: apply prettier

--- scripts/megaera.mjs
+++ scripts/megaera.mjs
@@ -1,5 +1,7 @@
 import 'zx/globals'
 
-await $({verbose: true})`megaera --schema .github/schema.graphql ${await glob('src/**/*.graphql')}`
+await $({
+  verbose: true,
+})`megaera --schema .github/schema.graphql ${await glob('src/**/*.graphql')}`
 await $`prettier --write ${await glob('src/**/*.graphql.ts')}`
 echo(chalk.green('TypeScript files generated successfully.'))
