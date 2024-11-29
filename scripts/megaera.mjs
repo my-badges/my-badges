83be72684fc7073a47b6575b5a87b8c635040cdf
Anton Golub <antongolub@antongolub.com>
2024-11-29 22:56:14 +0300

    refactor: rewrite repo fetcher with zurk

--- scripts/megaera.mjs
+++ scripts/megaera.mjs
@@ -1,5 +1,5 @@
 import 'zx/globals'
 
-await $`megaera --schema .github/schema.graphql ${await glob('src/**/*.graphql')}`.verbose()
+await $({verbose: true})`megaera --schema .github/schema.graphql ${await glob('src/**/*.graphql')}`
 await $`prettier --write ${await glob('src/**/*.graphql.ts')}`
 echo(chalk.green('TypeScript files generated successfully.'))
---
+++ test/repo.test.ts
@@ -0,0 +1,27 @@
+import * as assert from 'node:assert'
+import path from 'node:path'
+import fs from 'node:fs'
+import os from 'node:os'
+import { before, after, describe, it } from 'node:test'
+import { __internal__, syncRepo } from '../src/repo.js'
+
+const temp = `${os.tmpdir()}/${Math.random().toString(36).slice(2)}`
+fs.mkdirSync(temp)
+
+describe('repo API', () => {
+  before(() => __internal__.cwd = path.resolve(temp))
+  after(() => __internal__.cwd = process.cwd())
+
+  describe('syncRepo()', () => {
+    it('should sync the repo', () => {
+      const owner = 'webpod'
+      const repo = 'zurk'
+
+      syncRepo(owner, repo)
+      const pkgJson = JSON.parse(fs.readFileSync(path.resolve(temp, 'repo/package.json'), 'utf-8'))
+      assert.equal(pkgJson.name, repo)
+    })
+  })
+})
+
+after(() => fs.rmdirSync(temp, { recursive: true }))
