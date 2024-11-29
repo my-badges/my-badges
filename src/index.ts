b6f25a5c0aedd9da3652f17bb78bb4944e187e53
Anton Medvedev <anton@medv.io>
2024-11-29 14:12:22 +0100

    Fix reactions.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { User } from './task/user/user.graphql.js'
 export { Issue } from './task/issues/issues.graphql.js'
 export { PullRequest } from './task/pulls/pulls.graphql.js'
 export { Commit } from './task/commits/commits.graphql.js'
+export { Reactions } from './task/comments/comments.graphql.js'
 
 export { linkCommit, linkIssue, linkPull, latest, plural } from './utils.js'
