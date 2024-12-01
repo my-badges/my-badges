d2fe7ea7f84e44a04ffc4bbb7aa19ecc0250bb36
Anton Medvedev <anton@medv.io>
2024-12-01 23:39:45 +0100

    up/down

--- badges/thumbs-down/thumbs-down.ts
+++ badges/thumbs-down/thumbs-down.ts
@@ -7,9 +7,12 @@ type Where = {
 
 export default define({
   url: import.meta.url,
-  badges: ['confused'] as const,
+  tiers: true,
+  badges: ['thumbs-down-10', 'thumbs-down-50', 'thumbs-down-100'] as const,
   present(data, grant) {
     const moreThan10: Where[] = []
+    const moreThan50: Where[] = []
+    const moreThan100: Where[] = []
 
     for (const x of [
       ...data.issues,
@@ -19,18 +22,34 @@ export default define({
     ]) {
       if (x.reactions && x.reactions.length > 0) {
         const counts = count(x.reactions)
-        if (counts.CONFUSED > 10) {
-          moreThan10.push({ count: counts.THUMBS_UP, url: x.url })
+        if (counts.THUMBS_DOWN > 10) {
+          moreThan10.push({ count: counts.THUMBS_DOWN, url: x.url })
+        } else if (counts.THUMBS_DOWN > 50) {
+          moreThan50.push({ count: counts.THUMBS_DOWN, url: x.url })
+        } else if (counts.THUMBS_DOWN > 100) {
+          moreThan100.push({ count: counts.THUMBS_DOWN, url: x.url })
         }
       }
     }
 
     moreThan10.sort((a, b) => b.count - a.count)
+    moreThan50.sort((a, b) => b.count - a.count)
+    moreThan100.sort((a, b) => b.count - a.count)
 
     if (moreThan10.length > 0) {
-      grant('confused', `I confused more than 10 people.`)
+      grant('thumbs-down-10', `I got more than 10 thumbs down.`)
         .evidence(text(moreThan10))
         .tier(1)
+    }
+    if (moreThan50.length > 0) {
+      grant('thumbs-down-50', `I got more than 50 thumbs down.`)
+        .evidence(text(moreThan50))
+        .tier(2)
+    }
+    if (moreThan100.length > 0) {
+      grant('thumbs-down-100', `I got more than 100 thumbs down.`)
+        .evidence(text(moreThan100))
+        .tier(3)
     }
   },
 })
@@ -55,7 +74,7 @@ function count(reactions: Reaction[] | undefined) {
 function text(entries: Where[]): string {
   const lines: string[] = []
   for (const where of entries) {
-    lines.push(`* <a href="${where.url}">${where.count} 😕</a>`)
+    lines.push(`* <a href="${where.url}">${where.count} 👍</a>`)
   }
   return lines.join('\n')
 }
--- badges/thumbs-up/thumbs-up.ts
+++ badges/thumbs-up/thumbs-up.ts
@@ -7,9 +7,12 @@ type Where = {
 
 export default define({
   url: import.meta.url,
-  badges: ['confused'] as const,
+  tiers: true,
+  badges: ['thumbs-up-10', 'thumbs-up-50', 'thumbs-up-100'] as const,
   present(data, grant) {
     const moreThan10: Where[] = []
+    const moreThan50: Where[] = []
+    const moreThan100: Where[] = []
 
     for (const x of [
       ...data.issues,
@@ -19,18 +22,34 @@ export default define({
     ]) {
       if (x.reactions && x.reactions.length > 0) {
         const counts = count(x.reactions)
-        if (counts.CONFUSED > 10) {
+        if (counts.THUMBS_UP > 10) {
           moreThan10.push({ count: counts.THUMBS_UP, url: x.url })
+        } else if (counts.THUMBS_UP > 50) {
+          moreThan50.push({ count: counts.THUMBS_UP, url: x.url })
+        } else if (counts.THUMBS_UP > 100) {
+          moreThan100.push({ count: counts.THUMBS_UP, url: x.url })
         }
       }
     }
 
     moreThan10.sort((a, b) => b.count - a.count)
+    moreThan50.sort((a, b) => b.count - a.count)
+    moreThan100.sort((a, b) => b.count - a.count)
 
     if (moreThan10.length > 0) {
-      grant('confused', `I confused more than 10 people.`)
+      grant('thumbs-up-10', `I got more than 10 thumbs up.`)
         .evidence(text(moreThan10))
         .tier(1)
+    }
+    if (moreThan50.length > 0) {
+      grant('thumbs-up-50', `I got more than 50 thumbs up.`)
+        .evidence(text(moreThan50))
+        .tier(2)
+    }
+    if (moreThan100.length > 0) {
+      grant('thumbs-up-100', `I got more than 100 thumbs up.`)
+        .evidence(text(moreThan100))
+        .tier(3)
     }
   },
 })
@@ -55,7 +74,7 @@ function count(reactions: Reaction[] | undefined) {
 function text(entries: Where[]): string {
   const lines: string[] = []
   for (const where of entries) {
-    lines.push(`* <a href="${where.url}">${where.count} 😕</a>`)
+    lines.push(`* <a href="${where.url}">${where.count} 👍</a>`)
   }
   return lines.join('\n')
 }
