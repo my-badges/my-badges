3792814dbea99d1518af251734d493a447f0d9a0
Anton Golub <antongolub@antongolub.com>
2024-12-04 16:12:09 +0300

    feat: add badges if readme has no marks (#86)

--- src/update-readme.ts
+++ src/update-readme.ts
@@ -3,6 +3,10 @@ import path from 'node:path'
 import { Badge } from './badges.js'
 import { quoteAttr } from './utils.js'
 
+const START_MARK = '<!-- my-badges start -->'
+const END_MARK = '<!-- my-badges end -->'
+const BADGE_SIZE = 64
+
 export function updateReadme(
   badges: Badge[],
   size: number | string,
@@ -25,38 +29,37 @@ function detectReadmeFilename(cwd: string): string {
 }
 
 export function generateReadme(
-  readmeContent: string,
+  content: string,
   badges: Badge[],
-  size: number | string = 64,
+  size: number | string = BADGE_SIZE,
 ) {
-  const startString = '<!-- my-badges start -->'
-  const endString = '<!-- my-badges end -->'
+  const start = content.indexOf(START_MARK)
+  const end = content.indexOf(END_MARK)
+  console.log('start', start, 'end', end)
+  if ((start === -1 && end !== -1) || (start !== -1 && end === -1))
+    throw new Error(
+      'Invalid README.md: both or none of the marks should be present',
+    )
 
-  let content = readmeContent
+  if (start > end)
+    throw new Error('Invalid README.md: start mark should be before end mark')
 
-  const start = content.indexOf(startString)
-  const end = content.indexOf(endString)
-  const needToAddNewLine = content[end + endString.length + 1] !== '\n'
+  const badgesHtml = badges
+    .map((badge) => {
+      const desc = quoteAttr(badge.desc)
+      // prettier-ignore
+      return `<a href="my-badges/${badge.id}.md"><img src="${badge.image}" alt="${desc}" title="${desc}" width="${parseInt(size + '')}"></a>`
+    })
+    .join('\n')
 
-  if (start !== -1 && end !== -1) {
-    content = content.slice(0, start) + content.slice(end + endString.length)
+  if (end === -1 && start === -1)
+    return `${START_MARK}
+${badgesHtml}
+${END_MARK}
 
-    const badgesHtml = badges
-      .map((badge) => {
-        const desc = quoteAttr(badge.desc)
-        // prettier-ignore
-        return `<a href="my-badges/${badge.id}.md"><img src="${badge.image}" alt="${desc}" title="${desc}" width="${parseInt(size + '')}"></a>`
-      })
-      .join('\n')
+${content}`
 
-    content =
-      content.slice(0, start) +
-      `${startString}\n` +
-      badgesHtml +
-      `\n${endString}` +
-      (needToAddNewLine ? '\n' : '') +
-      content.slice(start)
-  }
-
-  return content
+  return `${content.slice(0, start)}${START_MARK}
+${badgesHtml}
+${END_MARK}${content.slice(end + END_MARK.length)}`
 }
