0de5a4ac12d099a18c9fc334c2270717c007b003
Rignchen <rinchendemort@gmail.com>
2025-01-17 16:41:44 +0100

    test(favorite-word): create test script file

---
+++ badges/favorite-word/favorite-word.test.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect, afterAll } from 'vitest'
+import fs from 'node:fs/promises'
+import favoriteWord from './favorite-word.js'
+import os from 'node:os'
+import { log } from '../../src/log.js'
+import { Badge } from '../../src/badges.js'
+import { Data } from '../../src/data.js'
+
+describe.skip('favorite-word', () => {
+  // prettier-ignore
+  describe('ignore conventional commit prefixes', () => {})
+})
+
