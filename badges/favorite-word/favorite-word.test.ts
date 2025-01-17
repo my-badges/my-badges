6a14284de42234f5e4e229cb747e674ea7eac961
Rignchen <rinchendemort@gmail.com>
2025-01-17 17:09:50 +0100

    test(favorite-word): add factory to create Data with just commit message & body

--- badges/favorite-word/favorite-word.test.ts
+++ badges/favorite-word/favorite-word.test.ts
@@ -5,6 +5,100 @@ import os from 'node:os'
 import { log } from '../../src/log.js'
 import { Badge } from '../../src/badges.js'
 import { Data } from '../../src/data.js'
+import { Commit } from '../../src/task/commits/commits.graphql.js'
+
+function CommitFactory(message: string, messageBody: string) {
+  const commit: Commit = {
+    message,
+    messageBody,
+    id: '',
+    sha: '',
+    committedDate: '',
+    additions: 0,
+    deletions: 0,
+    author: null,
+    committer: null,
+    repository: {
+      owner: {
+      login: '',
+      },
+      name: '',
+    },
+  }
+  return commit
+}
+
+function DataFactory(commits: Commit[]) {
+  const data: Data = {
+    repos: [
+      {
+        commits,
+        id: '',
+        name: '',
+        owner: {
+        login: '',
+        },
+        url: '',
+        description: null,
+        createdAt: '',
+        updatedAt: '',
+        languages: null,
+        forks: {
+        totalCount: 0,
+        },
+        stargazers: {
+        totalCount: 0,
+        },
+        defaultBranchRef: null,
+        isEmpty: false,
+      },
+    ],
+    starredRepositories: [],
+    pulls: [],
+    issues: [],
+    issueComments: [],
+    discussionComments: [],
+    user: {
+      id: '',
+      login: '',
+      name: '',
+      avatarUrl: '',
+      bio: '',
+      company: '',
+      location: '',
+      email: '',
+      twitterUsername: null,
+      websiteUrl: null,
+      status: null,
+      createdAt: '',
+      followers: {
+        totalCount: 0,
+      },
+      following: {
+        totalCount: 0,
+      },
+      anyPinnableItems: false,
+      pinnedItems: {
+        totalCount: 0,
+        nodes: null
+      },
+      sponsoring: {
+        totalCount: 0,
+      },
+      sponsors: {
+        totalCount: 0,
+      },
+      starredRepositories: {
+        totalCount: 0,
+      },
+      publicKeys: {
+        totalCount: 0,
+        nodes: null
+      },
+    }
+  }
+  return data
+}
 
 describe.skip('favorite-word', () => {
   // prettier-ignore
