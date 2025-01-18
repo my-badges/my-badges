3061b1b066067251d800e5716537d47300f02a5d
Rignchen <rinchendemort@gmail.com>
2025-01-18 21:34:34 +0100

    test(conventionnal-commit): work on smaller functions

--- badges/conventional-commit/conventional-commit.test.ts
+++ badges/conventional-commit/conventional-commit.test.ts
@@ -1,102 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import conventionalCommit from './conventional-commit.js'
-import { makeBadgeBody } from './conventional-commit.js'
-import { countBadgeType } from './conventional-commit.js'
-import { Data } from '../../src/data.js'
-import { Commit } from '../../src/task/commits/commits.graphql.js'
-import { Badge } from '../../src/badges.js'
-import { Evidence } from '../../src/badges.js'
-
-function CommitFactory(message: string, messageBody: string) {
-  return {
-    message,
-    messageBody,
-    id: '',
-    sha: '',
-    committedDate: '',
-    additions: 0,
-    deletions: 0,
-    author: null,
-    committer: null,
-    repository: {
-      owner: {
-        login: '',
-      },
-      name: '',
-    },
-  } as Commit
-}
-
-function DataFactory(commits: Commit[]) {
-  return {
-    repos: [
-      {
-        commits,
-        id: '',
-        name: '',
-        owner: {
-          login: '',
-        },
-        url: '',
-        description: null,
-        createdAt: '',
-        updatedAt: '',
-        languages: null,
-        forks: {
-          totalCount: 0,
-        },
-        stargazers: {
-          totalCount: 0,
-        },
-        defaultBranchRef: null,
-        isEmpty: false,
-      },
-    ],
-    starredRepositories: [],
-    pulls: [],
-    issues: [],
-    issueComments: [],
-    discussionComments: [],
-    user: {
-      id: '',
-      login: '',
-      name: '',
-      avatarUrl: '',
-      bio: '',
-      company: '',
-      location: '',
-      email: '',
-      twitterUsername: null,
-      websiteUrl: null,
-      status: null,
-      createdAt: '',
-      followers: {
-        totalCount: 0,
-      },
-      following: {
-        totalCount: 0,
-      },
-      anyPinnableItems: false,
-      pinnedItems: {
-        totalCount: 0,
-        nodes: null,
-      },
-      sponsoring: {
-        totalCount: 0,
-      },
-      sponsors: {
-        totalCount: 0,
-      },
-      starredRepositories: {
-        totalCount: 0,
-      },
-      publicKeys: {
-        totalCount: 0,
-        nodes: null,
-      },
-    },
-  } as Data
-}
+import { makeBadgeBody, countBadgeType } from './conventional-commit.js'
 
 describe('conventional-commit', () => {
   // prettier-ignore
@@ -115,34 +18,6 @@ describe('conventional-commit', () => {
     'test',
   ]
 
-  describe(`detect if conventional commit messages are used`, () => {
-    const run = (data: Data, shouldHaveBadge: boolean) => {
-      // Prepare
-      let hasBadge = false
-      // Act
-      conventionalCommit.present(data, (id: "conventional-commit", desc: string) => {
-        hasBadge = true
-        expect(desc).toBe("I use conventional commit messages")
-        return new Evidence({ id: 'conventional-commit', desc: '', body: '', image: '', tier: 0 })
-      })
-      // Assert
-      expect(hasBadge).toBe(shouldHaveBadge)
-    }
-    it('should not have a badge', () => {run(DataFactory([
-      CommitFactory('Hello World', ''),
-    ]), false)})
-    prefixes.forEach((prefix) => {
-      it(`should have a badge (${prefix})`, () => {run(DataFactory([
-        CommitFactory('Hello World', ''),
-        CommitFactory(`${prefix}: Hello World`, ''),
-      ]), true)})
-      it(`should have a badge (${prefix} with score)`, () => {run(DataFactory([
-        CommitFactory('Hello World', ''),
-        CommitFactory(`${prefix}(Hello): World`, ''),
-      ]), true)})
-    })
-  })
-
   prefixes.forEach((prefix) => {
     describe(`count the number of "${prefix}" prefixes`, () => {
       it(`should have a count of 1`, () => {
@@ -151,7 +26,7 @@ describe('conventional-commit', () => {
           `${prefix}: Hello World`,
         ])).toStrictEqual([[prefix, 1]])
       })
-      it(`should have a count of 1`, () => {
+      it(`should have a count of 3`, () => {
         expect(countBadgeType([
           'Hello World',
           `${prefix}: Hello World`,
@@ -164,45 +39,31 @@ describe('conventional-commit', () => {
   })
 
   it('Should work with multiple types of commits', () => {
-    // Prepare
-    let badge: Badge = { id: 'conventional-commit', desc: '', body: '', image: '', tier: 0 }
-    // Act
-    conventionalCommit.present(DataFactory([
-      CommitFactory('Hello World', ''),
-      CommitFactory('feat: Hello World', ''),
-      CommitFactory('fix: Hello World', ''),
-      CommitFactory('BREAKING CHANGE: Hello World', ''),
-      CommitFactory('fix: Hello World', ''),
-    ]), (id: "conventional-commit", desc: string) => {
-      return new Evidence(badge)
-    })
-    // Assert
-    expect(badge.body).toBe([
-      'I\'ve done 2 fix commit',
-      'I\'ve done 1 feature commit',
-      'I\'ve done 1 breaking change commit',
-    ].join('\n'))
+    expect(countBadgeType([
+      'Hello World',
+      'feat: Hello World',
+      'fix: Hello World',
+      'BREAKING CHANGE: Hello World',
+      'fix: Hello World',
+    ])).toStrictEqual([
+      ['feat', 1],
+      ['fix', 2],
+      ['BREAKING CHANGE', 1],
+    ])
   })
 
   it('Should count "!" as a breaking change', () => {
-    // Prepare
-    let badge: Badge = { id: 'conventional-commit', desc: '', body: '', image: '', tier: 0 }
-    // Act
-    conventionalCommit.present(DataFactory([
-      CommitFactory('feat!: Hello World', ''),
-      CommitFactory('feat: Hello World', ''),
-      CommitFactory('fix: Hello World', ''),
-      CommitFactory('BREAKING CHANGE!: Hello World', ''),
-      CommitFactory('BREAKING CHANGE: Hello World', ''),
-    ]), (id: "conventional-commit", desc: string) => {
-      return new Evidence(badge)
-    })
-    // Assert
-    expect(badge.body).toBe([
-      'I\'ve done 3 breaking change commit',
-      'I\'ve done 2 feature commit',
-      'I\'ve done 1 fix commit',
-    ].join('\n'))
+    expect(countBadgeType([
+      'feat!: Hello World',
+      'feat: Hello World',
+      'fix: Hello World',
+      'BREAKING CHANGE!: Hello World',
+      'BREAKING CHANGE: Hello World',
+    ])).toStrictEqual([
+      ['feat', 2],
+      ['BREAKING CHANGE', 3],
+      ['fix', 1],
+    ])
   })
 
   describe("check that the badge's message looks nice", () => {
