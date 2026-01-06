ed6cc3fbb31407beb9a6a9155b1268085660a545
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-01-06 14:55:31 +0200

    Add badge for `St. Patricks Day`

---
+++ badges/st-patricks-day/st-patricks-day.ts
@@ -0,0 +1,25 @@
+import { Commit, define, latest } from '#src'
+
+export default define({
+  url: import.meta.url,
+  badges: ['st-patricks-day'] as const,
+  present(data, grant) {
+    const commits: Commit[] = []
+
+    for (const repo of data.repos) {
+      for (const commit of repo.commits) {
+        const data = new Date(commit.committedDate)
+        if (data.getMonth() === 2 && data.getDate() === 17) {
+          commits.push(commit)
+        }
+      }
+    }
+
+    if (commits.length > 0) {
+      grant(
+        'st-patricks-day',
+        'I committed on St. Patrick\'s Day!',
+      ).evidenceCommits(...commits.sort(latest).slice(0, 6))
+    }
+  },
+})
